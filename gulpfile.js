4d12addc7e168de932be55893ad6dd4b932e19cd
Alena Chernova <alenvlasovav@gmail.com>
2023-09-24 13:20:58 +0300

    Исправляет по замечаниям наставника

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ export const styles = () => {
       csso()
     ]))
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
@@ -135,7 +135,6 @@ export const build = gulp.series(
     styles,
     html,
     scripts,
-    copyImages,
     createWebp,
     svgOptimize,
     sprite
@@ -151,7 +150,6 @@ export default gulp.series(
     styles,
     html,
     scripts,
-    copyImages,
     createWebp,
     svgOptimize,
     sprite,
